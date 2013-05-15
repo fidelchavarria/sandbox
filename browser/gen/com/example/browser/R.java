ddebf049079f4328f2fed017663d8187a4ff0eca
Fidel Chavarria <fidel.chavarria@excelsys.net>
2013-05-15 11:30:32 -0500

    working browser

--- browser/gen/com/example/browser/R.java
+++ browser/gen/com/example/browser/R.java
@@ -24,9 +24,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Go=0x7f080002;
+        public static final int Go=0x7f080001;
         public static final int URL=0x7f080000;
-        public static final int WebEngine=0x7f080001;
+        public static final int WebEngine=0x7f080002;
         public static final int action_settings=0x7f080003;
     }
     public static final class layout {
--- browser/src/com/example/browser/MainActivity.java
+++ browser/src/com/example/browser/MainActivity.java
@@ -1,40 +1,55 @@
 package com.example.browser;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
 	/** Called when the activity is first created. */
-	EditText URLText;
-	Button GoButton;
-	WebView Browser;
+	EditText uRLText;
+	Button goButton;
+	WebView browser;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		URLText = (EditText) findViewById(R.id.URL);
-		GoButton = (Button) findViewById(R.id.Go);
-		Browser = (WebView) findViewById(R.id.WebEngine);
-
-		GoButton.setOnClickListener(new OnClickListener() {
+		uRLText = (EditText) findViewById(R.id.URL);
+		goButton = (Button) findViewById(R.id.Go);
+		browser = (WebView) findViewById(R.id.WebEngine);
+		browser.setWebViewClient(new MyWebViewClient());
+		
+		goButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				Browser.loadUrl("http://" + URLText.getText().toString());
+				browser.loadUrl("http://" + uRLText.getText().toString());
 			}
 		});
 	}
 	
-//	private class MyWebViewClient extends WebViewClient {
-//		@Override
-//		public boolean shouldOverrideUrlLoading(WebView view, String url) {
-//			view.loadUrl(url);
-//			return true;
-//		}
-//	}
+	private class MyWebViewClient extends WebViewClient {
+
+		    @Override
+		    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+		        view.loadUrl(url);
+		        return true;
+		    }
+
+		    @Override
+		    public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+		        if ((event.getKeyCode() == KeyEvent.KEYCODE_BACK) && view.canGoBack()) {
+		            view.goBack();
+		            return true;
+		        }
+
+		        return super.shouldOverrideKeyEvent(view, event);
+		    }
+		}
 }
