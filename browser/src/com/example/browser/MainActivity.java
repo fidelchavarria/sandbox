44e035d9c6d6cf20942f94369f128448d7283e12
Fidel Chavarria <fidel.chavarria@excelsys.net>
2013-05-15 11:02:56 -0500

    functional web browser

--- browser/src/com/example/browser/MainActivity.java
+++ browser/src/com/example/browser/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,29 +14,27 @@ public class MainActivity extends Activity {
 	Button GoButton;
 	WebView Browser;
 
-	private class MyWebViewClient extends WebViewClient {
-		@Override
-		public boolean shouldOverrideUrlLoading(WebView view, String url) {
-			view.loadUrl(url);
-			return true;
-		}
-	}
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.main);
 
 		URLText = (EditText) findViewById(R.id.URL);
 		GoButton = (Button) findViewById(R.id.Go);
 		Browser = (WebView) findViewById(R.id.WebEngine);
-		Browser.loadUrl("http://www.google.com/");
 
 		GoButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				Browser.setWebViewClient(new MyWebViewClient());
 				Browser.loadUrl("http://" + URLText.getText().toString());
 			}
 		});
 	}
+	
+//	private class MyWebViewClient extends WebViewClient {
+//		@Override
+//		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//			view.loadUrl(url);
+//			return true;
+//		}
+//	}
 }
